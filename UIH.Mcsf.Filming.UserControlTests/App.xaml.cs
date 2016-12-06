2ef9b0f662201d97746947c237f8284c2aaeb605
whhub <wanghui122004@me.com>
2016-12-06 16:44:58 +0800

    [FilmingLocalSolution][Verison008] New Project UIH.Mcsf.Filming.UserControlTests

---
+++ UIH.Mcsf.Filming.UserControlTests/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace UIH.Mcsf.Filming.UserControlTests
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ UIH.Mcsf.Filming.UserControlTests/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace UIH.Mcsf.Filming.UserControlTests
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        // TODO-working-on: ViewModelLocater
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
