5f9c1918114f17d455a76ee1be73e70bcaa3556a
whhub <wanghui122004@me.com>
2016-12-06 16:36:33 +0800

    [FilmingLocalSolution][Verison007] New Project UIH.Mcsf.Filming.Adapters   &  new  class ViewerControlAdapter with 7 TODO-New-Feature

---
+++ UIH.Mcsf.Filming.Adapters/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UIH.Mcsf.Filming.Adapters")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UIH.Mcsf.Filming.Adapters")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("48327486-6352-49a0-8df1-3574f3108027")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UIH.Mcsf.Filming.Adapters/ViewerControlAdapter.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace UIH.Mcsf.Filming.Adapters
+{
+    /// <summary>
+    ///     Interaction logic for ViewerControlAdapter.xaml
+    /// </summary>
+    public partial class ViewerControlAdapter
+    {
+        public ViewerControlAdapter()
+        {
+            InitializeComponent();
+        }
+
+        // TODO-New-Feature: Layout Dependency Property
+
+        // TODO-New-Feature: Focus
+
+        // TODO-New-Feature: Selection
+
+        // TODO-New-Feature: Cells Dependecy Property
+
+        // TODO-New-Feature: ViewPort Layout Dependency Property
+
+        // TODO-New-Feature: regularLayout & irregularLayout
+
+        // TODO-New-Feature: MultiFormatCell
+    }
+}
