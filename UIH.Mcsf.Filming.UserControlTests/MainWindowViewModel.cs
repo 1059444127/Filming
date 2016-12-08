09b66c893389e70e2050c406170356309855d0b8
whhub <wanghui122004@me.com>
2016-12-08 15:01:17 +0800

    [FilmingLocalSolution][Verison022][UserControlTest] MainWindow.InputBindings  KeyBinding(F5)   StartTestCommand  :   An Entry For View & ViewModel test   &  Added TODO: Create available Layout

--- UIH.Mcsf.Filming.UserControlTests/MainWindowViewModel.cs
+++ UIH.Mcsf.Filming.UserControlTests/MainWindowViewModel.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 using UIH.Mcsf.Filming.ViewModel;
 
 namespace UIH.Mcsf.Filming.UserControlTests
 {
     public class MainWindowViewModel
     {
+        #region [--UserControlViewModel--]
+
         private object _userControlViewModel;
 
         public object UserControlViewModel
@@ -19,9 +25,40 @@ namespace UIH.Mcsf.Filming.UserControlTests
             return _001CreateViewerControlAdapterViewModel();
         }
 
+        #endregion [--UserControlViewModel--]
+
+        #region [--StartTestCommand--]
+
+        private ICommand _startTestCommand;
+
+        public ICommand StartTestCommand
+        {
+            get { return _startTestCommand = _startTestCommand ?? new RelayCommand(StartTest); }
+        }
+
+        private void StartTest()
+        {
+            _001ViewerControlAdapterTest();
+        }
+
+        #endregion [--StartTestCommand--]    
+
+        #region [--UserControl Test--]
+
         private object _001CreateViewerControlAdapterViewModel()
         {
             return new ViewerControlAdapterViewModel();
         }
+
+        private void _001ViewerControlAdapterTest()
+        {
+            var viewModel = _userControlViewModel as ViewerControlAdapterViewModel;
+            // TODO£ºCreate available Layout
+            viewModel.Layout = null;
+
+            MessageBox.Show("Hello F5");
+        }
+
+        #endregion [--UserControl Test--]
     }
 }
