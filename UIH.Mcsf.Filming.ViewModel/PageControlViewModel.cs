65e6abeb87f56e8f8ce8ff972c7404407d86bb1d
whhub <wanghui122004@me.com>
2016-12-15 11:53:16 +0800

    [FilmingLocalSolution][Version059] Added TODO-user-Intent ; PageControlViewModel Info UT  (mixed logic)

--- UIH.Mcsf.Filming.ViewModel/PageControlViewModel.cs
+++ UIH.Mcsf.Filming.ViewModel/PageControlViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using GalaSoft.MvvmLight;
 using UIH.Mcsf.Filming.Interfaces;
-using UIH.Mcsf.Filming.Model;
 
 namespace UIH.Mcsf.Filming.ViewModel
 {
@@ -22,6 +21,8 @@ namespace UIH.Mcsf.Filming.ViewModel
             }
         }
 
+        #endregion [--Layout--]
+
         #region [--ImageCells--]
 
         private IList<ImageCell> _imageCells;
@@ -41,7 +42,7 @@ namespace UIH.Mcsf.Filming.ViewModel
 
         #endregion [--ImageCells--]
 
-        #endregion [--ImageCells--]
+        #region [--Patient Info--]
 
         #region [--PatientName--]
 
@@ -60,10 +61,119 @@ namespace UIH.Mcsf.Filming.ViewModel
 
         #endregion [--PatientName--]
 
+        #region [--PatientID--]
+
+        private string _patientID;
+
+        public string PatientID
+        {
+            get { return _patientID; }
+            set
+            {
+                if (_patientID == value) return;
+                _patientID = value;
+                RaisePropertyChanged(() => PatientID);
+            }
+        }
+
+        #endregion [--PatientID--]
+
+        #region [--PatientAge--]
+
+        private string _patientAge;
+
+        public string PatientAge
+        {
+            get { return _patientAge; }
+            set
+            {
+                if (_patientAge == value) return;
+                _patientAge = value;
+                RaisePropertyChanged(() => PatientAge);
+            }
+        }
+
+        #endregion [--PatientAge--]
+
+        #region [--PatientSex--]
+
+        private string _patientSex;
+
+        public string PatientSex
+        {
+            get { return _patientSex; }
+            set
+            {
+                if (_patientSex == value) return;
+                _patientSex = value;
+                RaisePropertyChanged(() => PatientSex);
+            }
+        }
+
+        #endregion [--PatientSex--]
+
+
+        #endregion [--Patient Info--]
+
+        #region [--Study Info--]
+
+        #region [--StudyInstanceUID--]
+
+        private string _studyInstanceUID;
+
+        public string StudyInstanceUID
+        {
+            get { return _studyInstanceUID; }
+            set
+            {
+                if (_studyInstanceUID == value) return;
+                _studyInstanceUID = value;
+                RaisePropertyChanged(() => StudyInstanceUID);
+            }
+        }
+
+        #endregion [--StudyInstanceUID--]
+
+        #region [--StudyDate--]
+
+        private string _studyDate;
+
+        public string StudyDate
+        {
+            get { return _studyDate; }
+            set
+            {
+                if (_studyDate == value) return;
+                _studyDate = value;
+                RaisePropertyChanged(() => StudyDate);
+            }
+        }
+
+        #endregion [--StudyDate--]
+
+        #region [--AccessionNumber--]
+
+        private string _accessionNumber;
+
+        public string AccessionNumber
+        {
+            get { return _accessionNumber; }
+            set
+            {
+                if (_accessionNumber == value) return;
+                _accessionNumber = value;
+                RaisePropertyChanged(() => AccessionNumber);
+            }
+        }
+
+        #endregion [--AccessionNumber--]
+
+
+        #endregion [--Study Info--]
 
         private void RefreshTitle()
         {
-            
+            PatientName = sampleCellsampleCell.PatientName;
         }
     }
 }
---
+++ UIH.Mcsf.Filming.ViewModel_UT/PageControlViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UIH.Mcsf.Filming.ViewModel_UT
+{
+    [TestClass]
+    public class PageControlViewModelTests
+    {
+        // TODO-User-Intent: When StudyInstanceUID is mixed Then AccessionNumber is StarString
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        // TODO-User-Intent: When Cells have mixed PatientID Then PatientID is StarString
+        // TODO-User-Intent: When Cells except Empty Cells have same PatientID Then PatientID is same with sampleCell
+        // TODO-User-Intent: When Cells have mixed PatientName Then PatientName is Mixed
+        // TODO-User-Intent: When Cells except Empty Cells have same PatientName Then PatientName is same with sampleCell
+        // TODO-User-Intent: When Cells have mixed PatientAge Then PatientAge is StarString
+        // TODO-User-Intent: When Cells except Empty Cells have same PatientAge Then PatientAge is same with sampleCell
+        // TODO-User-Intent: When Cells have mixed PatientSex Then PatientSex is StarString
+        // TODO-User-Intent: When Cells except Empty Cells have same PatientSex Then PatientSex is same with sampleCell
+        // TODO-User-Intent: When Cells have mixed StudyInstanceUID Then StudyInstanceUID is StarString
+        // TODO-User-Intent: When Cells except Empty Cells have same StudyInstanceUID Then StudyInstanceUID is same with sampleCell
+        // TODO-User-Intent: When Cells have mixed StudyDate Then StudyDate is StarString
+        // TODO-User-Intent: When Cells except Empty Cells have same StudyDate Then StudyDate is same with sampleCell
+        // TODO-User-Intent: When Cells have mixed AccessionNumber Then AccessionNumber is StarString
+        // TODO-User-Intent: When Cells except Empty Cells have same AccessionNumber Then AccessionNumber is same with sampleCell
+    }
+}
