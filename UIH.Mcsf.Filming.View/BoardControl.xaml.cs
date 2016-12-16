66794caaddef7bd99c4e0e57e6b00f0209bb8b7f
whhub <wanghui122004@me.com>
2016-12-16 16:07:27 +0800

    [FilmingLocalSolution][Version069] Added CardControl To Test  BoardControl.DisplayMode

--- UIH.Mcsf.Filming.View/BoardControl.xaml.cs
+++ UIH.Mcsf.Filming.View/BoardControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace UIH.Mcsf.Filming.View
+﻿using System.Windows;
+
+namespace UIH.Mcsf.Filming.View
 {
     /// <summary>
     ///     Interaction logic for BoardControl.xaml
@@ -10,8 +12,39 @@
             InitializeComponent();
         }
 
-        //TODO: BoardControl.Dependency Property : DisplayMode
+        //TODO-working-on: BoardControl.Dependency Property : DisplayMode
+
+
+        public int DisplayMode
+        {
+            get { return (int)GetValue(DisplayModeProperty); }
+            set { SetValue(DisplayModeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DisplayMode.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DisplayModeProperty =
+            DependencyProperty.Register("DisplayMode", typeof(int), typeof(BoardControl), new PropertyMetadata(1));
+
+        
+
 
         //TODO: BoardControl.Dependency Property : Pages
+
+        #region Overrides of FrameworkElement
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            if (e.Property == DisplayModeProperty)
+            {
+                SetDisplayMode(DisplayMode);
+            }
+        }
+
+        #endregion
+
+        private void SetDisplayMode(int displayMode)
+        {
+            
+        }
     }
 }
