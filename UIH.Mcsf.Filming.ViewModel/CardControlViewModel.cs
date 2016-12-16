60c77ab0452ab0d2ebbd06f0126b9a794db85e68
whhub <wanghui122004@me.com>
2016-12-16 18:02:20 +0800

    [FilmingLocalSolution][Version073] BoardControl.Pages

--- UIH.Mcsf.Filming.ViewModel/CardControlViewModel.cs
+++ UIH.Mcsf.Filming.ViewModel/CardControlViewModel.cs
@@ -1,4 +1,6 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Collections.Generic;
+using GalaSoft.MvvmLight;
+using UIH.Mcsf.Filming.Interfaces;
 
 namespace UIH.Mcsf.Filming.ViewModel
 {
@@ -20,6 +22,23 @@ namespace UIH.Mcsf.Filming.ViewModel
         }
 
         #endregion [--DisplayMode--]
+
+        #region [--Pages--]
+
+        private IList<PageModel> _pages;
+
+        public IList<PageModel> Pages
+        {
+            get { return _pages; }
+            set
+            {
+                if (_pages == value) return;
+                _pages = value;
+                RaisePropertyChanged(() => Pages);
+            }
+        }
+
+        #endregion [--Pages--]
 
     }
 }
