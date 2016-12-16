de5c97188109fbda95c35b8ef894c0c6cc65782c
whhub <wanghui122004@me.com>
2016-12-16 15:15:15 +0800

    [FilmingLocalSolution][Version068] Working-on : BoardControl ,  Added 2 TODOs:          //TODO: BoardControl.Dependency Property : DisplayMode
            //TODO: BoardControl.Dependency Property : Pages

---
+++ UIH.Mcsf.Filming.View/BoardControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace UIH.Mcsf.Filming.View
+{
+    /// <summary>
+    ///     Interaction logic for BoardControl.xaml
+    /// </summary>
+    public partial class BoardControl
+    {
+        public BoardControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
