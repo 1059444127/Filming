d81e888ab582669992b61409884042b3ccf3bb41
whhub <wanghui122004@me.com>
2016-12-16 10:59:05 +0800

    [FilmingLocalSolution][Version064] PageControl.TitleBar   Add  PageIndex   &  ElementIndexConverter

---
+++ UIH.Mcsf.Filming.Converters/ElementIndexConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UIH.Mcsf.Filming.Converters
+{
+    public class ElementIndexConverter : IMultiValueConverter
+    {
+        #region Implementation of IMultiValueConverter
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.Assert(values.Length == 2);
+            return string.Format("{0}/{1}", values[0], values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
