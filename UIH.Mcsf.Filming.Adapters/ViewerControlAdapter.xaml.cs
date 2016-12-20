08692303e009f494462d7068ebd666497176aa76
whhub <wanghui122004@me.com>
2016-12-20 14:23:57 +0800

    [FilmingLocalSolution][Version079] Test PageModel in CardControlViewModel & 未展示出胶片的原因之二： 虚函数OnPropertyChanged在构造函数中调用了

--- UIH.Mcsf.Filming.Adapters/ViewerControlAdapter.xaml.cs
+++ UIH.Mcsf.Filming.Adapters/ViewerControlAdapter.xaml.cs
@@ -18,60 +18,26 @@ namespace UIH.Mcsf.Filming.Adapters
         {
             InitializeComponent();
             // TODO-later: ViewerControl Configure Path From Class Configure
-            ViewerControl.InitializeWithoutCommProxy(@"D:/UIH/appdata/filming/config/");//Configure.Environment.Instance.ApplicationPath);
+            ViewerControl.InitializeWithoutCommProxy(@"D:/UIH/appdata/filming/config/");
+                //Configure.Environment.Instance.ApplicationPath);
             ViewerControl.CanSelectCellByLeftClick = false; //Disable inner mouse left button down events.
         }
 
-
-
-        public Layout Layout
+        private void SetLayout()
         {
-            get { return (Layout)GetValue(LayoutProperty); }
-            set
-            {
-                SetValue(LayoutProperty, value);
-            }
-        }
-
-
-        // Using a DependencyProperty as the backing store for Layout.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty LayoutProperty =
-            DependencyProperty.Register("Layout", typeof(Layout), typeof(ViewerControlAdapter));
-
-
-        #region Overrides of FrameworkElement
-
-        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
-        {
-            base.OnPropertyChanged(e);
-            var property = e.Property;
-            if (property == LayoutProperty)
-            {
-                Layout.Setup(ViewerControl.LayoutManager);
-                CompleteCells();
-                return;
-            }
-            if (property == ImageCellsProperty)
-            {
-                FillCells();
-            }
-        }
-
-        private void RegisterEvent()
-        {
-            
+            Layout.Setup(ViewerControl.LayoutManager);
+            CompleteCells();
         }
 
         private void FillCells()
         {
             var cellCount = Math.Min(ImageCells.Count, ViewerControl.CellCount);
-            for (int i = 0; i < cellCount; i++)
+            for (var i = 0; i < cellCount; i++)
             {
                 var controlCell = ViewerControl.Cells.ElementAt(i) as FilmingControlCell;
                 Debug.Assert(controlCell != null);
                 controlCell.FillImage(ImageCells[i]);
             }
-
         }
 
         private void CompleteCells()
@@ -83,7 +49,7 @@ namespace UIH.Mcsf.Filming.Adapters
             // cell count is less
             // TODO-later: New FilmingControlCell(), not smell good
             var cells = new List<FilmingControlCell>();
-            for (int i = 0; i < deltaCellCount; i++)
+            for (var i = 0; i < deltaCellCount; i++)
             {
                 cells.Add(new FilmingControlCell());
             }
@@ -92,14 +58,13 @@ namespace UIH.Mcsf.Filming.Adapters
             // cell count is more,  not move, for performance conside ( ViewerControl only have interface to remove a cell, then refresh )
 
             // Register CellImpl.MouseDown Event
-            for (int i = 0; i < deltaCellCount; i++)
+            for (var i = 0; i < deltaCellCount; i++)
             {
                 var cellImpl = cells[i].Control;
                 Debug.Assert(cellImpl != null);
                 cellImpl.MouseDown -= CellImplOnMouseDown;
                 cellImpl.MouseDown += CellImplOnMouseDown;
             }
-
         }
 
         private void CellImplOnMouseDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
@@ -113,18 +78,35 @@ namespace UIH.Mcsf.Filming.Adapters
                 mouseButtonEventArgs.RightButton == MouseButtonState.Pressed));
         }
 
+        #region [--LayoutProperty--]
 
-        #endregion
+        public Layout Layout
+        {
+            get { return (Layout) GetValue(LayoutProperty); }
+            set { SetValue(LayoutProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Layout.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LayoutProperty =
+            DependencyProperty.Register("Layout", typeof (Layout), typeof (ViewerControlAdapter),
+                new PropertyMetadata(OnLayoutPropertyChanged));
+
+        private static void OnLayoutPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var viewerControlAdapter = d as ViewerControlAdapter;
+            Debug.Assert(viewerControlAdapter != null);
+
+            viewerControlAdapter.SetLayout();
+        }
+
+        #endregion [--LayoutProperty--]
 
         // TODO-New-Feature: ViewerControlAdapter.OnDrop (Original: use VisualTreeHelper to Location)
- 
-
-
 
 
         // TODO-New-Feature: ControlCell.Clone
 
-        
+
         // TODO-User-intent-later: Focus Status Stored in UI Layer
 
         // TODO-New-Feature: Remove Page
@@ -132,18 +114,28 @@ namespace UIH.Mcsf.Filming.Adapters
         // TODO-New-Feature: Select with up/down/left/right in a viewport
 
         // TODO-Later: ViewerControl.ImageCells and Memory Leak
+
+        #region [--ImageCellsProperty--]
+
         public IList<ImageCell> ImageCells
         {
-            get { return (IList<ImageCell>)GetValue(ImageCellsProperty); }
+            get { return (IList<ImageCell>) GetValue(ImageCellsProperty); }
             set { SetValue(ImageCellsProperty, value); }
         }
 
         // Using a DependencyProperty as the backing store for ImageCells.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ImageCellsProperty =
-            DependencyProperty.Register("ImageCells", typeof(IList<ImageCell>), typeof(ViewerControlAdapter));
+            DependencyProperty.Register("ImageCells", typeof (IList<ImageCell>), typeof (ViewerControlAdapter),
+                new PropertyMetadata(OnImageCellsPropertyChanged));
 
-        
-        
+        private static void OnImageCellsPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var viewerControlAdapter = d as ViewerControlAdapter;
+            Debug.Assert(viewerControlAdapter != null);
 
+            viewerControlAdapter.FillCells();
+        }
+
+        #endregion [--ImageCellsProperty--]
     }
 }
--- UIH.Mcsf.Filming.View/BoardControl.xaml.cs
+++ UIH.Mcsf.Filming.View/BoardControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -12,11 +13,15 @@ namespace UIH.Mcsf.Filming.View
     /// </summary>
     public partial class BoardControl
     {
+        private int _displayMode;
+        //TODO-later: Page Management in BoardControl
+        private readonly List<PageControl> _pages = new List<PageControl>();
+
         public BoardControl()
         {
             InitializeComponent();
 
-            for (int i = 0; i < GlobalDefinitions.MaxDisplayMode; i++)
+            for (var i = 0; i < GlobalDefinitions.MaxDisplayMode; i++)
             {
                 var pageControl = new PageControl();
                 _pages.Add(pageControl);
@@ -24,62 +29,11 @@ namespace UIH.Mcsf.Filming.View
             }
         }
 
-        //TODO-later: Page Management in BoardControl
-        private List<PageControl> _pages = new List<PageControl>(); 
-
-        //TODO-working-on: BoardControl.Dependency Property : DisplayMode
-
-
-        public int DisplayMode
-        {
-            get { return (int)GetValue(DisplayModeProperty); }
-            set { SetValue(DisplayModeProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for DisplayMode.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty DisplayModeProperty =
-            DependencyProperty.Register("DisplayMode", typeof(int), typeof(BoardControl));
-
-
-
-        //TODO: BoardControl.Dependency Property : Pages
-
-
-
-        public IList<PageModel> PageModels
-        {
-            get { return (IList<PageModel>)GetValue(PageModelsProperty); }
-            set { SetValue(PageModelsProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for PageModels.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty PageModelsProperty =
-            DependencyProperty.Register("PageModels", typeof(IList<PageModel>), typeof(BoardControl));
-
-        private int _displayMode;
-
-        #region Overrides of FrameworkElement
-
-        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
-        {
-            if (e.Property == DisplayModeProperty)
-            {
-                SetDisplayMode();
-                return;
-            }
-            if (e.Property == PageModelsProperty)
-            {
-                FillPages();
-            }
-        }
-
-        #endregion
-
         // TODO-working-on: BoardControl.FillPages
         private void FillPages()
         {
-            int i = 0;
-            while (i<PageModels.Count && i<_displayMode)
+            var i = 0;
+            while (i < PageModels.Count && i < _displayMode)
             {
                 var pageControl = _pages[i];
                 pageControl.Visibility = Visibility.Visible;
@@ -89,7 +43,7 @@ namespace UIH.Mcsf.Filming.View
                 pageControl.DataContext = new PageControlViewModel(pageModel);
                 i++;
             }
-            while (i<GlobalDefinitions.MaxDisplayMode)
+            while (i < GlobalDefinitions.MaxDisplayMode)
             {
                 var pageControl = _pages[i];
                 pageControl.Visibility = Visibility.Hidden;
@@ -106,8 +60,8 @@ namespace UIH.Mcsf.Filming.View
 
         private void SetGrid()
         {
-            int row = _displayMode%2==0 ? 2 : 1;
-            int col = _displayMode/row;
+            var row = _displayMode%2 == 0 ? 2 : 1;
+            var col = _displayMode/row;
             SetGrid(row, col);
         }
 
@@ -120,11 +74,12 @@ namespace UIH.Mcsf.Filming.View
 
         private void PlacePagesToGrid(int row, int col)
         {
+            //TODO-later: Page Size Control
             var scale = new ScaleTransform(1.0/col, 1.0/row);
             var pageIndex = 0;
-            for (int i = 0; i < row; i++)
+            for (var i = 0; i < row; i++)
             {
-                for (int j = 0; j < col; j++)
+                for (var j = 0; j < col; j++)
                 {
                     var page = _pages[pageIndex];
                     Grid.SetRow(page, i);
@@ -156,5 +111,58 @@ namespace UIH.Mcsf.Filming.View
             if (rowDelta < 0) rows.RemoveRange(row, -rowDelta);
             if (colDelta < 0) cols.RemoveRange(col, -colDelta);
         }
+
+        //TODO-working-on: BoardControl.Dependency Property : DisplayMode
+
+        #region [--DisplayModeProperty--]
+
+        public int DisplayMode
+        {
+            get { return (int) GetValue(DisplayModeProperty); }
+            set { SetValue(DisplayModeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DisplayMode.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DisplayModeProperty =
+            DependencyProperty.Register("DisplayMode", typeof (int), typeof (BoardControl),
+                new PropertyMetadata(OnDisplayModePropertyChanged));
+
+        private static void OnDisplayModePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var boardControl = d as BoardControl;
+            Debug.Assert(boardControl != null);
+
+            var displayMode = (int) e.NewValue;
+            Debug.Assert(displayMode >= 0 && displayMode < GlobalDefinitions.MaxDisplayMode);
+
+            boardControl.SetDisplayMode();
+        }
+
+        #endregion [--DisplayModeProperty--]
+
+        //TODO: BoardControl.Dependency Property : Pages
+
+        #region [--PageModelsProperty--]
+
+        public IList<PageModel> PageModels
+        {
+            get { return (IList<PageModel>) GetValue(PageModelsProperty); }
+            set { SetValue(PageModelsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PageModels.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PageModelsProperty =
+            DependencyProperty.Register("PageModels", typeof (IList<PageModel>), typeof (BoardControl),
+                new PropertyMetadata(OnPageModelsPropertyChanged));
+
+        private static void OnPageModelsPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var boardControl = d as BoardControl;
+            Debug.Assert(boardControl != null);
+
+            boardControl.FillPages();
+        }
+
+        #endregion  [--PageModelsProperty--]
     }
 }
