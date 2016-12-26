8c9968b324405fd4b42b0ec273b602eb56d73c54
whhub <wanghui122004@me.com>
2016-12-26 10:28:44 +0800

    [FilmingLocalSolution][Version097][Test]  Add TestMethod For // TODO-User-intent: New_Page_When_Board_Control_Is_Not_Fullfilled_Then_New_Page_Is_Displayed_at_the_end

--- UIH.Mcsf.Filming.UT/BoardModelTests.cs
+++ UIH.Mcsf.Filming.UT/BoardModelTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UIH.Mcsf.Filming.Interfaces;
 
 namespace UIH.Mcsf.Filming.UT
 {
@@ -6,6 +8,19 @@ namespace UIH.Mcsf.Filming.UT
     public class BoardModelTests
     {
         // TODO-User-intent: New_Page_When_Board_Control_Is_Full_Of_Page_Control_Then_New_Page_Is_Invisible
-        // TODO-User-intent: New_Page_When_Board_Control_Is_Not_Fullfilled_Then_New_Page_Is_Displayed_at_the_end
+        // TODO-User-intent-working-on: New_Page_When_Board_Control_Is_Not_Fullfilled_Then_New_Page_Is_Displayed_at_the_end
+        [TestMethod]
+        public void New_Page_When_Board_Control_Is_Not_Fullfilled_Then_New_Page_Is_Displayed_at_the_end()
+        {
+            // Arrange
+            var boardModel = new BoardModel();
+
+            // Act
+            boardModel.NewPage();
+
+            // Assert
+            var pageModel = boardModel.PageModels.FirstOrDefault();
+            Assert.IsTrue(pageModel!= null && pageModel.IsVisible);
+        }
     }
 }
