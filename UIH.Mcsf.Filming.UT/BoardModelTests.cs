83f56bbc4fbeb48a06ef8bccade4064e66ff35dd
whhub <wanghui122004@me.com>
2016-12-26 10:51:44 +0800

    [FilmingLocalSolution][Version101] Change BoardTest:  New Page always Diplayed at the end

--- UIH.Mcsf.Filming.UT/BoardModelTests.cs
+++ UIH.Mcsf.Filming.UT/BoardModelTests.cs
@@ -7,16 +7,10 @@ namespace UIH.Mcsf.Filming.UT
     [TestClass]
     public class BoardModelTests
     {
-        // TODO-User-intent: New_Page_When_Board_Control_Is_Full_Of_Page_Control_Then_New_Page_Is_Invisible
-        [TestMethod]
-        public void New_Page_When_Board_Control_Is_Full_Of_Page_Control_Then_New_Page_Is_Invisible()
-        {
-            // Assert
 
-        }
-        // TODO-User-intent-working-on: New_Page_When_Board_Control_Is_Not_Fullfilled_Then_New_Page_Is_Displayed_at_the_end
+        // TODO-User-intent-working-on: New_Page_Is_Always_Displayed_at_the_end
         [TestMethod]
-        public void New_Page_When_Board_Control_Is_Not_Fullfilled_Then_New_Page_Is_Displayed_at_the_end()
+        public void New_Page_Is_Always_Displayed_at_the_end()
         {
             // Arrange
             var boardModel = new BoardModel();
