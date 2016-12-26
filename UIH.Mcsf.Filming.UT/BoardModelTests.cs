4ec8a25c3f1b1271044defa3373c1e2e6f0ceb72
whhub <wanghui122004@me.com>
2016-12-26 10:19:51 +0800

    [FilmingLocalSolution][Version096][Test] Add Two user intent about BoardControl.NewPage

---
+++ UIH.Mcsf.Filming.UT/BoardModelTests.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UIH.Mcsf.Filming.UT
+{
+    [TestClass]
+    public class BoardModelTests
+    {
+        // TODO-User-intent: New_Page_When_Board_Control_Is_Full_Of_Page_Control_Then_New_Page_Is_Invisible
+        // TODO-User-intent: New_Page_When_Board_Control_Is_Not_Fullfilled_Then_New_Page_Is_Displayed_at_the_end
+    }
+}
