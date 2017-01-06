fa9ab1b759425fc355d2a23f6ac4eb8ab0a374a4
whhub <wanghui122004@me.com>
2017-01-06 14:16:33 +0800

    [FilmingLocalSolution][Version177] Add 3 TODOs in IBoardCell  1)         // TODO: Change PageModelChanged Type From EventHandler<PageModelEventArgs> to EventHandler   2)         // TODO: Add IBoardCell.Row   3)         // TODO: Add IBoardCell.Col

--- UIH.Mcsf.Filming.Interfaces/IBoardCell.cs
+++ UIH.Mcsf.Filming.Interfaces/IBoardCell.cs
@@ -5,7 +5,16 @@ namespace UIH.Mcsf.Filming.Interfaces
     public interface IBoardCell
     {
         bool IsVisible { set; }
+        
         PageModel PageModel { get; set; }
+        // TODO: Change PageModelChanged Type From EventHandler<PageModelEventArgs> to EventHandler
         event EventHandler<PageModelEventArgs> PageModelChanged;
+        
+        // TODO: Add IBoardCell.Row
+        // TODO: Add IBoardCell.Col
+        //int Row { get; set; }
+        //event EventHandler RowChanged;
+        //int Col { get; set; }
+        //event EventHandler ColChanged;
     }
 }
