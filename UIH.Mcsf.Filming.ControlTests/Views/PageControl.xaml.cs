4a37831c9302bfcf1873855af818cd1bc0e3718a
whhub <wanghui122004@me.com>
2017-01-11 10:51:53 +0800

    [FilmingLocalSolution][Version194] Create PageControl  in Project ControlTests // To Replace  ContentControl in DynamicGrid

---
+++ UIH.Mcsf.Filming.ControlTests/Views/PageControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace UIH.Mcsf.Filming.ControlTests.Views
+{
+    // TODO: Replace ContentControl in DynamicGrid with PageControl
+    /// <summary>
+    ///     Interaction logic for PageControl.xaml
+    /// </summary>
+    public partial class PageControl
+    {
+        public PageControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
