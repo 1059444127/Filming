5ede6803ef49bc5d73d82c7a73b46fdafa53771f
whhub <wanghui122004@me.com>
2017-01-12 16:49:13 +0800

    [FilmingLocalSolution][Version202][refactor] remove Row & Col From  IGridCell

--- UIH.Mcsf.Filming.ControlTests/ViewModel/DynamicGridViewModel.cs
+++ UIH.Mcsf.Filming.ControlTests/ViewModel/DynamicGridViewModel.cs
@@ -10,14 +10,14 @@ namespace UIH.Mcsf.Filming.ControlTests.ViewModel
         {
             GridCellViewModels = new List<IGridCell>
             {
-                new PageControlViewModel {Row = 0, Col = 0, TitleBarViewModel = new TitleBarViewModel {PageNO = 1, PageCount = 8}},
-                new PageControlViewModel {Row = 0, Col = 1, TitleBarViewModel = new TitleBarViewModel {PageNO = 2, PageCount = 8}},
-                new PageControlViewModel {Row = 0, Col = 2, TitleBarViewModel = new TitleBarViewModel {PageNO = 3, PageCount = 8}},
-                new PageControlViewModel {Row = 0, Col = 3, TitleBarViewModel = new TitleBarViewModel {PageNO = 4, PageCount = 8}},
-                new PageControlViewModel {Row = 1, Col = 0, TitleBarViewModel = new TitleBarViewModel {PageNO = 5, PageCount = 8}},
-                new PageControlViewModel {Row = 1, Col = 1, TitleBarViewModel = new TitleBarViewModel {PageNO = 6, PageCount = 8}},
-                new PageControlViewModel {Row = 1, Col = 2, TitleBarViewModel = new TitleBarViewModel {PageNO = 7, PageCount = 8}},
-                new PageControlViewModel {Row = 1, Col = 3, TitleBarViewModel = new TitleBarViewModel {PageNO = 8, PageCount = 8}}
+                new PageControlViewModel {TitleBarViewModel = new TitleBarViewModel {PageNO = 1, PageCount = 8}},
+                new PageControlViewModel {TitleBarViewModel = new TitleBarViewModel {PageNO = 2, PageCount = 8}},
+                new PageControlViewModel {TitleBarViewModel = new TitleBarViewModel {PageNO = 3, PageCount = 8}},
+                new PageControlViewModel {TitleBarViewModel = new TitleBarViewModel {PageNO = 4, PageCount = 8}},
+                new PageControlViewModel {TitleBarViewModel = new TitleBarViewModel {PageNO = 5, PageCount = 8}},
+                new PageControlViewModel {TitleBarViewModel = new TitleBarViewModel {PageNO = 6, PageCount = 8}},
+                new PageControlViewModel {TitleBarViewModel = new TitleBarViewModel {PageNO = 7, PageCount = 8}},
+                new PageControlViewModel {TitleBarViewModel = new TitleBarViewModel {PageNO = 8, PageCount = 8}}
             };
         }
 
@@ -49,10 +49,6 @@ namespace UIH.Mcsf.Filming.ControlTests.ViewModel
 
             MessageBox.Show("Make Page 6 Invisible");
             GridCellViewModels[5].Visibility = Visibility.Collapsed;
-
-            MessageBox.Show("Move Page 3 to Position 6");
-            GridCellViewModels[2].Col = 1;
-            GridCellViewModels[2].Row = 1;
         }
 
         #endregion
@@ -61,7 +57,5 @@ namespace UIH.Mcsf.Filming.ControlTests.ViewModel
     public interface IGridCell
     {
         Visibility Visibility { get; set; }
-        int Row { get; set; }
-        int Col { get; set; }
     }
 }
--- UIH.Mcsf.Filming.ControlTests/ViewModel/PageControlViewModel.cs
+++ UIH.Mcsf.Filming.ControlTests/ViewModel/PageControlViewModel.cs
@@ -31,41 +31,6 @@ namespace UIH.Mcsf.Filming.ControlTests.ViewModel
 
         #endregion [--Visibility--]
 
-        #region [--Row--]
-
-        private int _row;
-
-        public int Row
-        {
-            get { return _row; }
-            set
-            {
-                if (_row == value) return;
-                _row = value;
-                RaisePropertyChanged(() => Row);
-            }
-        }
-
-        #endregion [--Row--]
-
-        #region [--Col--]
-
-        private int _col;
-
-        public int Col
-        {
-            get { return _col; }
-            set
-            {
-                if (_col == value) return;
-                _col = value;
-                RaisePropertyChanged(() => Col);
-            }
-        }
-
-        #endregion [--Col--]
-
-
         #endregion
 
         public TitleBarViewModel TitleBarViewModel { get; set; }
