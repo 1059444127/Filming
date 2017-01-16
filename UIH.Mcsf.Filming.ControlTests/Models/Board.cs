da5aedb7cb30128f638133e1a0e4c76c92e73726
whhub <wanghui122004@me.com>
2017-01-16 16:38:57 +0800

    [FilmingLocalSolution][Version212] ControlTest.Board

---
+++ UIH.Mcsf.Filming.ControlTests/Models/Board.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UIH.Mcsf.Filming.ControlTests.Interfaces;
+
+namespace UIH.Mcsf.Filming.ControlTests.Models
+{
+    public class Board : IBoard
+    {
+        #region Implementation of IBoard
+
+        public int CellCount
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler CellCountChanged = delegate { };
+
+        public IList<BoardCell> BoardCells
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public void NewPage()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
