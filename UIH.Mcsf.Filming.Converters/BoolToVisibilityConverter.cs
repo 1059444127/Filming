1bc98b1f37b3514865898eeafa0c85e2bba2803c
whhub <wanghui122004@me.com>
2017-01-18 17:10:45 +0800

    [FilmingLocalSolution][Version228] Add BoolToVisibilityConverter

---
+++ UIH.Mcsf.Filming.Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UIH.Mcsf.Filming.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        #region Implementation of IValueConverter
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isVisible = value is bool && (bool) value;
+            return isVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
