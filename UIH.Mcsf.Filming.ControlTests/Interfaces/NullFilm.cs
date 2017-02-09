e42c81c3e0b305358e5c8dc906ae94f790d6f32e
whhub <wanghui122004@me.com>
2017-02-09 16:00:01 +0800

    [FilmingLocalSolution][Verison270] Add NullFilm : IFilm

---
+++ UIH.Mcsf.Filming.ControlTests/Interfaces/NullFilm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UIH.Mcsf.Filming.ControlTests.Interfaces
+{
+    public class NullFilm : IFilm
+    {
+        #region Implementation of IFilm
+
+        public IFilmTitleSubject FilmTitle { get; private set; }
+
+        #endregion
+
+        #region Implementation of IPage
+
+        public bool IsVisible
+        {
+            get { return false; }
+            set { }
+        }
+
+        public event EventHandler VisibleChanged;
+
+        public ITitleSubject Title
+        {
+            get { return FilmTitle; }
+        }
+
+        #endregion
+    }
+}
