d5a102410a06b900f3b00ae277dd47e77e858f00
whhub <wanghui122004@me.com>
2017-02-10 17:22:24 +0800

    [FilmingLocalSolution][Verison280][user content] Change First_Film_in_Board_is_Visible  to Only_First_Film_in_Board_is_Visible

--- UIH.Mcsf.Filming.ControlTests_UT/BoardContentTests.cs
+++ UIH.Mcsf.Filming.ControlTests_UT/BoardContentTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UIH.Mcsf.Filming.ControlTests.Models;
+using UIH.Mcsf.Filming.Utilities;
 
 namespace UIH.Mcsf.Filming.ControlTests_UT
 {
@@ -15,11 +16,15 @@ namespace UIH.Mcsf.Filming.ControlTests_UT
         }
 
         [TestMethod]
-        public void First_Film_In_Board_Is_Visible()
+        public void Only_First_Film_In_Board_Is_Visible()
         {
 
             // Assert
-            Assert.IsTrue(_boardContent[0].IsVisible);
+            Assert.IsTrue(_boardContent[0].IsVisible, "First Film in Board is InVisible");
+            for (int i = 1; i < GlobalDefinitions.MaxDisplayMode; i++)
+            {
+                Assert.IsFalse(_boardContent[i].IsVisible, string.Format("Film {0} in Board is Visible", i));
+            }
         }
     }
 }
