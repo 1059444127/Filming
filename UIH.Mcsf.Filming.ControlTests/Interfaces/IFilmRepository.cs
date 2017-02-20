296395b396802052bd8d2da03c5e20aaf8cf6c54
whhub <wanghui122004@me.com>
2017-02-20 17:21:43 +0800

    [FilmingLocalSolution][Version303][Clean] inline interface IAppend to IFilmRepository

--- UIH.Mcsf.Filming.ControlTests/Interfaces/IFilmRepository.cs
+++ UIH.Mcsf.Filming.ControlTests/Interfaces/IFilmRepository.cs
@@ -2,12 +2,13 @@
 
 namespace UIH.Mcsf.Filming.ControlTests.Interfaces
 {
-    public interface IFilmRepository : IAppend
+    public interface IFilmRepository
     {
         ISelectableFilm this[int i] { get; }
         void Add(ISelectableFilm film);
 
         int Focus { get; set; }
         event EventHandler FocusChanged;
+        void Append();
     }
 }
