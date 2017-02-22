d7d829834755c91e3361eecdb9f71e8920594e43
whhub <wanghui122004@me.com>
2017-02-22 19:36:10 +0800

    [FilmingLocalSolution][Version311] FilmBuffer

---
+++ UIH.Mcsf.Filming.ControlTests/Models/FilmBuffer.cs
@@ -0,0 +1,19 @@
+﻿using UIH.Mcsf.Filming.ControlTests.Interfaces;
+using UIH.Mcsf.Filming.Utilities;
+
+namespace UIH.Mcsf.Filming.ControlTests.Models
+{
+    public class FilmBuffer
+    {
+        private IFilmRepository _films;
+        private const int Capacity = GlobalDefinitions.MaxDisplayMode;
+        private int _cursor;
+
+        public FilmBuffer(IFilmRepository filmRepository)
+        {
+            _films = filmRepository;
+        }
+
+        public int VisibleSize { get; set; }
+    }
+}
